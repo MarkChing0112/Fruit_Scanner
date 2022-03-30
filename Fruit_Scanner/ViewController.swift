aad5780ffee550b71820ee1f317aed8b61bb66b4
okokli5 <manho445@gmail.com>
2022-03-30 15:46:18 +0800

    Initial commit

---
+++ Fruit_Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fruit_Scanner
+//
+//  Created by kin ming ching on 30/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Fruit_Scanner
+ 4217_PJ&AI_PJ
