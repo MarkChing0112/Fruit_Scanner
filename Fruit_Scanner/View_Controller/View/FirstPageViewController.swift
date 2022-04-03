c04f5a3bd67def9ff10fbfa1b8295fbca4fffc40
okokli5 <manho445@gmail.com>
2022-04-03 17:37:39 +0800

    First_page_UI

--- Fruit_Scanner/View_Controller/View/FirstPageViewController.swift
+++ Fruit_Scanner/View_Controller/View/FirstPageViewController.swift
@@ -6,7 +6,32 @@
 //
 
 import UIKit
-
+import Foundation
+import Firebase
+import FirebaseAuth
 class FirstPageViewController: UIViewController {
 
+    @IBOutlet weak var SignOutBtn: UINavigationItem!
+    @IBAction func SignOutBtnOnTap(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do{
+        try firebaseAuth.signOut()
+              let firebaseAuth = Auth.auth()
+              print("signout success")
+              showAlertS()
+          } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+          }
+    }
+    func toHomeView(){
+        let namestoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = namestoryboard.instantiateViewController(withIdentifier: "LoginHomeViewController") as! LoginHomeViewController
+        view.window?.rootViewController = vc
+        view.window?.makeKeyAndVisible()
+    }
+    func showAlertS(){
+        let alert = UIAlertController(title: "SignOut Success!", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "ok!", style: .cancel, handler: {action in self.toHomeView()}))
+        present(alert, animated: true)
+    }
 }
