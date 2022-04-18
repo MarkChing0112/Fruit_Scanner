0274fefb7b8755e03c34115d8af468ea5c24b81b
okokli5 <manho445@gmail.com>
2022-04-18 16:40:45 +0800

    Ver:0.1.34
    Firestore data format update
    Added take photo form camera function
    Added button add photo withimage library

--- Fruit_Scanner/View_Controller/AI_View/FruitAIViewController.swift
+++ Fruit_Scanner/View_Controller/AI_View/FruitAIViewController.swift
@@ -16,6 +16,7 @@ import SwiftUI
 
 class FruitAIViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
     @IBOutlet weak var SaveBtn: UIButton!
+    @IBOutlet weak var RecordBtn: UIButton!
     //declear
     var FreshLevel : String!
     var fruit_Name : String!
@@ -27,7 +28,13 @@ class FruitAIViewController: UIViewController, UIImagePickerControllerDelegate, 
         formatter.dateFormat = "MM/dd/yyyy-HH:mm:a"
         return formatter
     }()
-
+    let formatter2: DateFormatter = {
+        let formatter2 = DateFormatter()
+        formatter2.timeZone = .current
+        formatter2.locale = .current
+        formatter2.dateFormat = "MM/dd/yyyy-HH:mm"
+        return formatter2
+    }()
     private var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "photo")
@@ -37,7 +44,7 @@ class FruitAIViewController: UIViewController, UIImagePickerControllerDelegate, 
     private let label: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
-        label.text = "Select Image"
+        label.text = ""
         label.numberOfLines = 0
         return label
     }()
@@ -57,15 +64,28 @@ class FruitAIViewController: UIViewController, UIImagePickerControllerDelegate, 
         view.addSubview(imageView)
         view.addSubview(Clabel)
         // Do any additional setup after loading the view.
-        let tap = UITapGestureRecognizer(
-            target: self,
-            action: #selector(didTapImage)
-        )
-        tap.numberOfTapsRequired = 1
-        imageView.isUserInteractionEnabled = true
-        imageView.addGestureRecognizer(tap)
+        //let tap = UITapGestureRecognizer(
+        //    target: self,
+        //    action: #selector(didTapImage)
+        //)
+        //tap.numberOfTapsRequired = 1
+        //imageView.isUserInteractionEnabled = true
+        //imageView.addGestureRecognizer(tap)
+        RecordBtn.alpha = 0
     }
     
+    @IBAction func TakePhotoBtnDipTap(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    @IBAction func SelectphotoBtnDidTap(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true)
+    }
     @objc func didTapImage() {
         let picker = UIImagePickerController()
         picker.sourceType = .photoLibrary
@@ -137,8 +157,10 @@ class FruitAIViewController: UIViewController, UIImagePickerControllerDelegate, 
         // cancelled
         picker.dismiss(animated: true, completion: nil)
     }
+    //var
     var checkJPG : Bool = true
     var checkPNG : Bool = true
+    //imagepicker
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true, completion: nil)
         guard let immage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
@@ -158,21 +180,23 @@ class FruitAIViewController: UIViewController, UIImagePickerControllerDelegate, 
             showAlert()
         }else{
             if(imageData.isEmpty){
+                RecordBtn.alpha = 1
                 showConfirmAlertpng(imageDataP: imageData1)
             }else{
+                RecordBtn.alpha = 1
                 showConfirmAlert(imageDataP: imageData)
             }
             
         }
     }
     
-    @IBAction func SaveBtnOnTap(_ sender: Any) {
-        if (imageView.image == UIImage(systemName: "photo")){
-            showAlert()
-        }else{
-           //showConfirmAlert()
-        }
-    }
+  //  @IBAction func SaveBtnOnTap(_ sender: Any) {
+  //  if (imageView.image == UIImage(systemName: "photo")){
+  //          showAlert()
+  //      }else{
+  //         //showConfirmAlert()
+  //      }
+  //  }
     
     //to RecordPage
     func toRecordPage(){
@@ -268,6 +292,7 @@ class FruitAIViewController: UIViewController, UIImagePickerControllerDelegate, 
                 //setdate
                 let date = Date()
                 let time1 = formatter.string(from: date)
+                let time2 = formatter2.string(from: date)
                 Ref.putData(imageData, metadata: nil){
                     _, error in
                     //check error
